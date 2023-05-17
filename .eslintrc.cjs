27c14e4b01050d58bc8ed4cbf54a985e704de58f
SABooks <sakim.books@gmail.com>
2023-05-17 20:07:23 +0900

    add github actions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
+    //'eslint:recommended',
+    'standard',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -11,5 +11,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'import/no-absolute-path': 'off'
   },
 }
