5a333e99cb2bbabae0c31f8dc3e4fb98c9addb9c
SABooks <sakim.books@gmail.com>
2023-05-17 20:26:24 +0900

    cross-env 설정
    개발과 운영 환경 분리 로직

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const insProd = process.env.NODE_ENV == 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   //github 저장소 이름
-  base: '/FEStudy_TicTacToe'
+  base: isProd ? '/FEStudy_TicTacToe' : '/'
 })
